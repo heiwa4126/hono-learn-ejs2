b0fc25e90387442979684c5291828620e7ba9cfa
heiwa4126 <heiwa4126@gmail.com>
2026-03-02 15:34:46 +0900

    update

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,22 @@ import { Hono } from "hono";
 
 export const app = new Hono();
 
-app.get("/", async (c) => {
-	const html = await ejs.renderFile("views/index.ejs", { title: "Hono" });
-	return c.html(html);
-});
+const template1 = "views/index.ejs";
+app
+	.get("/", async (c) => {
+		const html = await ejs.renderFile(template1, { count: 0 });
+		return c.html(html);
+	})
+	.post("/", async (c) => {
+		const form = await c.req.formData();
+		const action = form.get("action");
+		let count = Number(form.get("count")) || 0;
+
+		if (action === "increment") {
+			count++;
+		} else if (action === "reset") {
+			count = 0;
+		}
+		const html = await ejs.renderFile(template1, { count });
+		return c.html(html);
+	});
